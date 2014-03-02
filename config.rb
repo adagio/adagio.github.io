e08eb6cc6e617a9149e860f0e251d17ca3802a69
Franco Cedillo <franco.cedillo@gmail.com>
2014-03-02 18:42:37 -0500

    Adds ruby support to sass with Gemfile and config.rb

---
+++ config.rb
@@ -0,0 +1,12 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'sass'
+images_dir = 'images'
+#javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = false
+sass_options = {:sourcemap => true}
+
+output_style = 'expanded'
+#output_style = :compressed
